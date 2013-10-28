f5171024273ea358cec9c0b733be11ee6f971059
Ingimar Robertsson <iar42@server.fake>
2013-10-28 15:35:23 +0000

    Added column sorting options from Franck at linuxpourtous.com

--- all-cables.php
+++ all-cables.php
@@ -5,6 +5,10 @@ $tab['reports']['allcables'] = 'All Cables'; // title of the report tab
 
 function DisplayAllCables()
 {
+	$page		= 'index.php?page=reports&tab=allcables';
+	$direction	= (!empty($_REQUEST['direction'])) ? trim($_REQUEST['direction']) : 'DESC';
+	$order_by	= (!empty($_REQUEST['order_by'])) ? trim($_REQUEST['order_by']) .' '.$direction : 'L.cable DESC';
+
 	$query = 'SELECT L.cable AS cableid, O.name AS dev1, P.name AS port1, D.dict_value as type1, ' .
 	         '       O2.name AS dev2, P2.name as port2, D2.dict_value as type2 ' .
 	         'FROM Link as L ' .
@@ -14,27 +18,33 @@ function DisplayAllCables()
 	         'LEFT JOIN Object as O2 on O2.id = P2.object_id ' .
 	         'LEFT JOIN Dictionary as D on D.dict_key = P.type ' .
 	         'LEFT JOIN Dictionary as D2 on D2.dict_key = P2.type ' .
-	         'ORDER BY L.cable DESC';
+	         'ORDER BY ' . trim($order_by);
+
 	$result = usePreparedSelectBlade ($query);
 	$row = $result->fetch (PDO::FETCH_ASSOC);
+
+	$direction = ($direction == 'DESC') ? 'ASC' : 'DESC';
 
 	echo '<div class=portlet>';
 	echo "<h2>All Cables:</h2>";
 	echo '<table border=0 cellpadding=5 cellspacing=0 align=center class=cooltable>';
 	echo '<tr>';
-	echo '<th>Cable ID</th>';
-	echo '<th>Device 1</th>';
-	echo '<th>Port 1</th>';
-	echo '<th>Type 1</th>';
-	echo '<th>Device 2</th>';
-	echo '<th>Port 2</th>';
-	echo '<th>Type 2</th>';
+	echo '<th><a href="'.$page.'&order_by=cableid&direction='.$direction.'">Cable ID</a></th>';
+	echo '<th><a href="'.$page.'&order_by=dev1&direction='.$direction.'">Device 1</a></th>';
+	echo '<th><a href="'.$page.'&order_by=port1&direction='.$direction.'">Port 1</a></th>';
+	echo '<th><a href="'.$page.'&order_by=type1&direction='.$direction.'">Type 1</a></th>';
+	echo '<th><a href="'.$page.'&order_by=dev2&direction='.$direction.'">Device 2</a></th>';
+	echo '<th><a href="'.$page.'&order_by=port2&direction='.$direction.'">Port 2</a></th>';
+	echo '<th><a href="'.$page.'&order_by=type2&direction='.$direction.'">Type 2</a></th>';
 	echo '</tr>';
+
 	$count = 0;
-	$lastcableid = "";
+	$lastcableid = '';
+
         foreach ($result as $row)
         {
 		if( $lastcableid == $row['cableid'] ) { $count--; }
+
 		echo '<tr class="row_' . (++$count%2 ? "odd" : "even") . ' tdleft" valign=top>';
 		echo '<td><b>' . $row['cableid'] . '</b></td>';
 		echo '<td><b>' . $row['dev1'] . '</b></td>';
@@ -47,8 +57,8 @@ function DisplayAllCables()
 		echo '</tr>';
 		$lastcableid = $row['cableid'];
 	}
+
 	echo '</table>';
 	echo '</div>';
 }
-
 ?>
